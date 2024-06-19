25e123d1ae1e8a0dca15311bb2124a62d4a1bdb1
Abhishek <programwithabhi@gmail.com>
2024-06-19 20:22:27 +0530

    navbar and main dummy content added

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Navbar from "./home/Navbar";
+import Main from "./home/Main";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      I'm Abhishek passionate about computer science.
-    </main>
+    <>
+    <Navbar/>
+    <Main/>
+    </>
   );
 }
