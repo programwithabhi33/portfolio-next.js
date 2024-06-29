1ed9fabc516bd0f7ad1c3640b2e4837a3e6dcc76
Abhishek <programwithabhi@gmail.com>
2024-06-29 21:12:59 +0530

    replace the " with &quot;

--- src/app/home/Main.jsx
+++ src/app/home/Main.jsx
@@ -15,11 +15,11 @@ const Main = () => {
                                 <div className="flex flex-col items-center text-center justify-center">
                                     <h2 className="font-medium title-font mt-4 text-gray-900 text-lg">Abhishek Biranje</h2>
                                     <div className="w-12 h-1 bg-indigo-500 rounded mt-2 mb-4"></div>
-                                    <p className="text-base">Raclette knausgaard hella meggs normcore williamsburg enamel pin sartorial venmo tbh hot chicken gentrify portland.</p>
+                                    <p className="text-base">I&quot;m passionate about computer science</p>
                                 </div>
                             </div>
                             <div className="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
-                                <p className="leading-relaxed text-lg mb-4">Meggings portland fingerstache lyft, post-ironic fixie man bun banh mi umami everyday carry hexagon locavore direct trade art party. Locavore small batch listicle gastropub farm-to-table lumbersexual salvia messenger bag. Coloring book flannel truffaut craft beer drinking vinegar sartorial, disrupt fashion axe normcore meh butcher. Portland 90's scenester vexillologist forage post-ironic asymmetrical, chartreuse disrupt butcher paleo intelligentsia pabst before they sold out four loko. 3 wolf moon brooklyn.</p>
+                                <p className="leading-relaxed text-lg mb-4">I&quot;m passionate about computer science</p>
                             </div>
                         </div>
                     </div>
