f43acc86af23f958d37780719dce87629ea48e36
Abhishek <programwithabhi@gmail.com>
2024-12-10 20:28:27 +0530

    updated the .gitignore file to ignore .env file and created the environment variable of the database connection url

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export async function connectToDb() {
-  return await mongoose.connect('mongodb://192.168.0.110:27017/personalPortfolio');
+  return await mongoose.connect(process.env.MONGODB_CONNECTION_URL);
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
