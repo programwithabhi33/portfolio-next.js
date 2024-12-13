2097c88a107548008c9bc0bef578fb8102f1ea6e
Abhishek <programwithabhi@gmail.com>
2024-12-13 20:15:47 +0530

    Added the loading when clicked on the blog detail page and also added the not found content when the blog is not in the database

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,19 +1,20 @@
 import { getBlogArticlesFromSlug } from '@/app/server-actions/blogArticle'
 import React from 'react'
+import BlogNotFound from './BlogNotFound';
 
 const page = async ({ params }) => {
     const slug = await params.slug
     const blogArticle = await getBlogArticlesFromSlug(slug);
     return (
         <section className="text-gray-600 body-font overflow-hidden">
-            <div className="container px-5 py-24 mx-auto">
+            {blogArticle[0]?<div className="container px-5 py-24 mx-auto">
                 <div className="flex flex-wrap -m-12">
                     <div className="p-12 md:w-full flex flex-col items-start">
-                        <h2 className="sm:text-3xl text-2xl title-font font-medium text-gray-900 mt-4 mb-4">{blogArticle[0].title}</h2>
-                        <p className="leading-relaxed mb-8">{blogArticle[0].content}</p>
-                    </div>
+                        <h2 className="sm:text-3xl text-2xl title-font font-medium text-gray-900 mt-4 mb-4">{blogArticle[0]?.title}</h2>
+                        <p className="leading-relaxed mb-8">{blogArticle[0]?.content}</p>
+                    </div> 
                 </div>
-            </div>
+            </div>: <BlogNotFound/>}
         </section>
     )
 }
---
+++ src/app/blog/loading.jsx
@@ -0,0 +1,9 @@
+const loading = () => {
+  return (
+    <div className="flex align-center m-8 justify-center">
+      Loading... 
+    </div>
+  )
+}
+
+export default loading
