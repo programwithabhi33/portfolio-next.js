2d1eb186149bbbb9e358e3ca69b03373cb8fb27f
Abhishek <programwithabhi@gmail.com>
2024-12-20 19:25:45 +0530

    Added the overlow-x-auto in sub-blog to scroll the content horizontally if there is code in the article

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -7,12 +7,12 @@ const page = async ({ params }) => {
     const blogArticle = await getBlogArticlesFromSlug(slug);
     const markup = { __html: blogArticle[0]?.content }
     return (
-        <section className="text-gray-600 body-font overflow-hidden">
+        <section className="text-gray-600 body-font overflow-hidden overflow-x-auto">
             {blogArticle[0] ? <div className="container px-5 py-24 mx-auto">
                 <div className="flex flex-wrap">
                     <div className="md:p-12 md:w-full flex flex-col items-start flex-wrap">
                         <h2 className="sm:text-3xl text-2xl title-font font-medium text-gray-900 mt-4 mb-4">{blogArticle[0]?.title}</h2>
-                        <p dangerouslySetInnerHTML={markup} className="leading-relaxed mb-8"/>
+                        <div dangerouslySetInnerHTML={markup} className="flex flex-wrap leading-relaxed mb-8"/>
                     </div>
                 </div>
             </div> : <BlogNotFound />}
